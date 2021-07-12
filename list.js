84373e7d7177922ba766f4ca671f9b907970680e
Karen Aguirre <latingirl_2289@hotmail.com>
2021-07-12 20:14:05 +0200

    Reproductor de musica

---
+++ list.js
@@ -0,0 +1,32 @@
+let All_song = [
+    {
+      name: "Cara de luna / Bacilos",
+      path: "music/1.mp3",
+      img: "img/img1.jpg",
+      singer: "1"
+    },
+    {
+      name: "Raro / Chino & Nacho",
+      path: "music/2.mp3",
+      img: "img/img2.jpg",
+      singer: "2"
+    },
+    {
+      name: "Cola song / J Balvin & Inna",
+      path: "music/3.mp3",
+      img: "img/img3.jpg",
+      singer: "3"
+    },
+    {
+      name: "El polvorete / El Gallo sube",
+      path: "music/4.mp3",
+      img: "img/img4.jpg",
+      singer: "4"
+    },
+    {
+      name: "Te pintaron pajaritos / Andy Rivera & Yandar & Yostin",
+      path: "music/5.mp3",
+      img: "img/img5.jpg",
+      singer: "5"
+    }
+ ];
